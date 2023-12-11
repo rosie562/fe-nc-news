8606299f1ca580edaa32c8ecdc6a78a6ffaf4d2d
rosie562 <rosieclairegarrett@gmail.com>
2023-12-11 15:53:44 +0000

    adds SingleArticle component

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -1,13 +1,16 @@
 import { useState, useEffect } from "react";
 import { getAllArticles } from "../api";
 import { Link } from "react-router-dom";
+import SingleArticle from "./SingleArticle";
 
 export default function Articles() {
   const [articles, setArticles] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     getAllArticles()
       .then((articles) => {
+        setIsLoading(false);
         setArticles(articles);
       })
       .catch((err) => {
@@ -15,26 +18,16 @@ export default function Articles() {
       });
   }, []);
 
+  if (isLoading) return <p> Loading... </p>;
+
   return (
     <>
       <section className="border p-4">
         <h2 className="text-2xl font-bold mb-4">Articles:</h2>
-        <section className=" grid-cols-1 gap-4">
+        <section className="grid grid-cols-1 gap-4">
           {articles.map((article) => (
             <div className="border mt-3 mb-3" key={article.article_id}>
-              <Link to={`/${article.article_id}`}>
-                <p className="font-bold p-2 pl-3 pt-3 text-2.5xl mt-2">
-                  {article.title}
-                </p>
-              </Link>
-              <p className="font-bold pl-3 p-1">{article.author}</p>
-              <p className="pl-3 p-2 ">{article.created_at.substring(0, 10)}</p>
-              <p className=" pl-3 pb-3 p-1">{article.comment_count} comments</p>
-              {article.votes ? (
-                <p className=" pl-3 pb-3 p-1">{article.votes} votes</p>
-              ) : 
-                ""
-              }
+              <SingleArticle article={article} />
             </div>
           ))}
         </section>
