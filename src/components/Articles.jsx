1fb47d8f2e1e361738250b413e4368bd2752c392
rosie562 <rosieclairegarrett@gmail.com>
2023-12-11 15:15:07 +0000

    moves key to correct line in map function

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -21,14 +21,20 @@ export default function Articles() {
         <h2 className="text-2xl font-bold mb-4">Articles:</h2>
         <section className=" grid-cols-1 gap-4">
           {articles.map((article) => (
-            <div className="border mt-3 mb-3">
-              <Link to={`/${article.article_id}`} key={article.article_id}>
-                <p className="font-bold p-2 pl-3 pt-3 text-2.5xl mt-2">{article.title}</p>
+            <div className="border mt-3 mb-3" key={article.article_id}>
+              <Link to={`/${article.article_id}`}>
+                <p className="font-bold p-2 pl-3 pt-3 text-2.5xl mt-2">
+                  {article.title}
+                </p>
               </Link>
               <p className="font-bold pl-3 p-1">{article.author}</p>
               <p className="pl-3 p-2 ">{article.created_at.substring(0, 10)}</p>
               <p className=" pl-3 pb-3 p-1">{article.comment_count} comments</p>
-              {article.votes ? <p className=" pl-3 pb-3 p-1">{article.votes} votes</p> : ''}
+              {article.votes ? (
+                <p className=" pl-3 pb-3 p-1">{article.votes} votes</p>
+              ) : 
+                ""
+              }
             </div>
           ))}
         </section>
